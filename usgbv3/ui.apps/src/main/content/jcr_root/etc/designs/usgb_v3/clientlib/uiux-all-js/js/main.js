d96a72271742a51003bc4e6ac3b8ba4d5b724c45
NBK-16-079\User <vincent@apdgroup.com>
2018-06-04 10:17:05 +0800

    - 60% merge

--- usgbv3/ui.apps/src/main/content/jcr_root/etc/designs/usgb_v3/clientlib/uiux-all-js/js/main.js
+++ usgbv3/ui.apps/src/main/content/jcr_root/etc/designs/usgb_v3/clientlib/uiux-all-js/js/main.js
@@ -241,7 +241,12 @@ function viewport() {
             var $this = $(this);
 
 
-            $this.addClass('open').find($('.mega-dropdown-menu')).css({"z-index": calc() });
+            $this.addClass('open').find($('.mega-dropdown-menu')).css({"z-index": calc()});
+
+            //provide a decent scrollbar when mega menu out of screen
+            //setTimeout(delay, 1000);
+
+    
 
             /*
             if( $all.hasClass('open') ){
@@ -258,7 +263,8 @@ function viewport() {
             }
 
             function delay(){
-              $this.addClass('open').find($('.mega-dropdown-menu')).css({"z-index": calc() });
+              //$this.addClass('open').find($('.mega-dropdown-menu')).css({"z-index": calc() });
+             $('.mega-dropdown-menu').css({"overflow": 'auto'}); 
             }
 
                 
@@ -268,6 +274,15 @@ function viewport() {
 
                 
                 $all.removeClass('open');
+
+                //clear all inline style
+               // setTimeout(delayClear, 1000);
+               
+                //$('.mega-dropdown-menu').css({"overflow": 'hidden'}); 
+
+                function delayClear(){
+                 $('.mega-dropdown-menu').attr('style',''); 
+                }
 
             });
 
@@ -415,11 +430,16 @@ $(window).on('load', function() {
             minWidth: 0
         });
 
+        console.log('onload');
+
     }
 
 
-    if ($('.make-sticky').length)makeSticky(); 
-
+    if ($('.make-sticky').length){
+        
+        setTimeout(function(){ makeSticky(); }, 100); //for stupid cacat IE browser
+        
+    }
 });
 
 
@@ -430,22 +450,22 @@ $(window).on('load', function() {
     "use strict";
     $(document).ready(function(){
 
-        if ($('div[data-sub-menu="start"]').length > 0) { 
+        if ($('div[data-product-menu="start"]').length > 0) { 
            
             $(window).scroll(function(){    
-                var posSub = $(".product-sub-menu").offset().top -119;
-                var posSubEnd = $(".product-sub-menu-end").offset().top -300;
+                var posSub = $(".product-menu").offset().top - 120;
+                var posSubEnd = $(".product-menu-end").offset().top -300;
 
                     if ($(this).scrollTop() > posSub) {
-                        $('#sub-menu').fadeIn(600).slideDown(700);
+                        $('.product-menu-sticky').fadeIn(600).slideDown(700);
                     } else {
-                        $('#sub-menu').slideUp(700).fadeOut(600);
+                        $('.product-menu-sticky').slideUp(700).fadeOut(600);
                     }
 
                     if ($(this).scrollTop() > posSubEnd) {
-                        $('#sub-menu').addClass("hidden");
+                        $('.product-menu-sticky').addClass("hidden");
                     }else{
-                        $('#sub-menu').removeClass("hidden");
+                        $('.product-menu-sticky').removeClass("hidden");
                     }
             });
         }
@@ -458,14 +478,214 @@ $(window).on('load', function() {
 
 
 // ----------------------------------------------------------------------
+// Shaftliner Pop up content [not in use currently]
+// ----------------------------------------------------------------------
+// (function(){
+//     "use strict";
+//     $(document).ready(function(){
+//       //change image and content dynamic
+      
+//       var img_change 
+//         var img_change2 
+//         var title_shaft
+//         var desc_shaft  
+
+//         function  changeContent(){
+//             $('div[data-content="content-img1"] img').attr("src","images/"+img_change+ "");
+//             $('div[data-content="content-img2"] img').attr("src","images/"+img_change2+ "");
+//             $('div[data-content="content-text"] h2').text(title_shaft);
+//             $('div[data-content="content-text"] p').text(desc_shaft);
+//         }
+       
+//         //hide div first
+//         $("div[data-shaft-content]").hide();
+        
+//         //close btn
+//         $( "a[data-content='close-btn']" ).on( "click", function() {
+//             $("div[data-shaft-content]").slideUp();
+//         });
+
+
+//         // click to show content
+//         $( "div[data-shaft]" ).on( "click", function() {
+
+//             var getShaft = $( this ).attr( "data-shaft" );
+//             $("div[data-shaft-content]").slideDown();
+     
+
+//             if(getShaft == "content-1")
+//             {
+//                 img_change = "bg-banner-tile.jpg";
+//                 img_change2 = "bg-banner-tile.jpg";
+//                 title_shaft = "Shaftliner1 lorem ipsum";
+//                 desc_shaft  = "Lorem ipsum dolor sit amet, consectetuer. Lorem ipsum dolor sit amet, consectetuer. Lorem ipsum dolor sit amet, consectetuer dolor sit amet, consectetuer one";       
+//             }
+//             if(getShaft == "content-2")
+//             {
+//                 img_change = "bg-banner-tile-2.jpg";
+//                 img_change2 = "bg-banner-tile-2.jpg";
+//                 title_shaft = "Shaftliner2 lorem ipsum";
+//                 desc_shaft  = "Lorem ipsum dolor sit amet, consectetuer. Lorem ipsum dolor sit amet, consectetuer. Lorem ipsum dolor sit amet, consectetuer dolor sit amet, consectetuer two";
+//             }
+//             if(getShaft == "content-3")
+//             {
+//                 img_change = "bg-banner-tile-3.jpg";
+//                 img_change2 = "bg-banner-tile-3.jpg";
+//                 title_shaft = "Shaftliner3 lorem ipsum";
+//                 desc_shaft  = "Lorem ipsum dolor sit amet, consectetuer. Lorem ipsum dolor sit amet, consectetuer. Lorem ipsum dolor sit amet, consectetuer dolor sit amet, consectetuer three";
+//             }
+//             if(getShaft == "content-4")
+//             {
+//                 img_change = "bg-banner-tile.jpg";
+//                 img_change2 = "bg-banner-tile-2.jpg";
+//                 title_shaft = "Shaftliner4 lorem ipsum";
+//                 desc_shaft  = "Lorem ipsum dolor sit amet, consectetuer. Lorem ipsum dolor sit amet, consectetuer. Lorem ipsum dolor sit amet, consectetuer dolor sit amet, consectetuer 4";
+//             }
+//             if(getShaft == "content-5")
+//             {
+//                 img_change = "bg-banner-tile-2.jpg";
+//                 img_change2 = "bg-banner-tile-3.jpg";
+//                 title_shaft = "Shaftliner5 lorem ipsum";
+//                 desc_shaft  = "Lorem ipsum dolor sit amet, consectetuer. Lorem ipsum dolor sit amet, consectetuer. Lorem ipsum dolor sit amet, consectetuer dolor sit amet, consectetuer 5";
+//             }
+
+//             changeContent();
+//         });
+
+
+//     });
+// })();
+
+
+// ----------------------------------------------------------------------
+// Equal Height for contact us and thank you page
+// ----------------------------------------------------------------------
+
+
+// (function(){
+//     "use strict";
+//     $(document).ready(function(){
+//         if($(window).width() > 767){
+//             var highestBox = 0;
+//            $('.equal-height').each(function(){  
+//                if($(this).height() > highestBox){  
+//                    highestBox = $(this).height();  
+//                }
+//            });    
+//            $('.equal-height').height(highestBox);
+//        }else{
+//            var highestBox = 0;
+//            $('.equal-height').each(function(){  
+//                if($(this).height() > highestBox){  
+//                    highestBox = $(this).height();  
+//                }
+//            });    
+//        }
+//     });
+// })();
+
+
+
+
+// ----------------------------------------------------------------------
 // Match Height
 // ----------------------------------------------------------------------
 (function(){
     "use strict";
     $(document).ready(function(){
+        $('[data-img-height="div-height"]').matchHeight();   
         $('[data-img-height="img-height"]').matchHeight();   
 
     });
 })();
 
 
+
+// ----------------------------------------------------------------------
+//[TEST] Product details gallery slickjs thumbnail
+// ----------------------------------------------------------------------
+(function () {
+    "use strict";
+    $(document).ready(function () {
+
+
+        $('.product-gallery-wrapper').slick({
+            customPaging: function (slick, index) {
+                var image_title = slick.$slides.eq(index).find('img').attr('src') || '';
+                return '<img src=' + image_title + '> </img>';
+            },
+        });
+
+
+        $('.product-gallery-wrapper').slickLightbox({
+            closeOnBackdropClick: true,
+            slick: function ($e) {
+
+                $e.find('.slick-lightbox-slick-iframe').each(function () {
+                    $(this)
+                        .attr('data-src', $(this).attr('src'))
+                        .attr('src', '')
+                })
+
+                function clearIframe(slick, index) {
+                    var $iframe = $(slick.$slides.get(index)).find('.slick-lightbox-slick-iframe')
+                    if ($iframe.length) {
+                        setTimeout(function () {
+                            $iframe.attr('src', '')
+                        }, slick.options.speed)
+                    }
+                }
+
+                function loadIframe(slick, index) {
+                    var $iframe = $(slick.$slides.get(index)).find('.slick-lightbox-slick-iframe')
+                    if ($iframe.length) $iframe.attr('src', $iframe.attr('data-src'))
+                }
+
+            
+                 //Return slick instance
+                return $e.find('.slick-lightbox-slick')
+                    .on('init', function (event, slick) {
+                        loadIframe(slick, slick.currentSlide)
+                    })
+                    .on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+                        clearIframe(slick, currentSlide)
+                        loadIframe(slick, nextSlide)
+                    })
+                    .slick()
+            }
+        }); //lightbox end
+
+    });
+})();
+
+
+// ----------------------------------------------------------------------
+// Checkbox Count
+// ----------------------------------------------------------------------
+(function () {
+    "use strict";
+    $(document).ready(function () {
+
+        var countCheck = 0;
+        $(".popup-content").hide()
+
+        // detect change for checkbox
+        $('.listing-search-content div input[type="checkbox"]').change(function () {
+
+            var numberOfChecked = $('.listing-search-content div input:checkbox:checked').length;
+            $(".popup-content").show();
+
+            if (numberOfChecked > 3) {
+                $(this).prop('checked', false);
+                console.log("reached max 3")
+            }
+
+            if (numberOfChecked == 0) {
+                $(".popup-content").hide();
+            }
+
+        });
+
+
+    });
+})();
