6053b510edd3f043bdde1cedd85a437a7607fd50
Vincent <vincent@apdgroup.com>
2018-06-12 10:18:15 +0800

    Main banner - text alignment - test video background
    Mega Menu & header
    - Function completed, pending feedback

--- usgbv3/ui.apps/src/main/content/jcr_root/etc/designs/usgb_v3/clientlib/uiux-all-js/js/main.js
+++ usgbv3/ui.apps/src/main/content/jcr_root/etc/designs/usgb_v3/clientlib/uiux-all-js/js/main.js
@@ -365,6 +365,8 @@ function viewport() {
 (function(){
     "use strict";
     $(document).ready(function(){
+
+        //when click on [data-link-to] urls
         $("[data-link-to]").on( "click",  function() {         
             
             var $this = $(this);
@@ -372,20 +374,41 @@ function viewport() {
             var $allSections = $('.hamburger-content>.section');
 
             var $thisAttrVal = $this.attr('data-link-to');
+            var $thisAttrValReplace = $this.attr('data-link-to').replace(/\s/g,'');
 
             var $allContents = $("[data-link-to-content]");
-            var allContentsAttrValue = $allContents.attr('data-link-to-content');
+            //var allContentsAttrValue = $allContents.attr('data-link-to-content');
 
-            function compareAttrValue(){
+            function getAllAttrValue(){
+
+                var arr = [];
+
                 $allContents.each(function( index ) {
                   //console.log( index + ": " + $( this ).text() );
                   var $eachContent = $(this);
-                  var eachContentAttrValue = $eachContent.attr('data-link-to-content');
-                  console.log(eachContentAttrValue);
-                });
-            }
-            compareAttrValue();
+                  var eachContentAttrValue = $eachContent.attr('data-link-to-content').replace(/\s/g,'');
+                  //console.log(eachContentAttrValue);
 
+                  arr.push(eachContentAttrValue);
+                });
+
+                return arr;
+            }
+
+
+            if( $.inArray($thisAttrValReplace, getAllAttrValue() ) !== -1){
+                console.log( getAllAttrValue() );
+                console.log( $thisAttrValReplace );
+
+                //hide/reset all levels
+                $allSections.addClass('hidden');
+                
+                //show content
+               $('.hamburger-content>.section[data-link-to-content="'+$thisAttrVal+'"]').removeClass('hidden');
+            }
+
+            //console.log(compareAttrValue.eachContentAttrValue);
+            
             //console.log("This attr: " + $thisAttrVal);
             //console.log("All attr: " + $allAttrVal);
 
@@ -398,6 +421,84 @@ function viewport() {
 
             //console.log($thisAttrVal);
         });
+
+
+        //when click on [data-link-back-to] urls
+        $("[data-link-back-to]").on( "click",  function() { 
+           
+            console.log('link back to');
+            
+            var $this = $(this);
+
+            var $allSections = $('.hamburger-content>.section');
+
+
+            //if has parent associated, revert to level 2, level 3 ......
+            if( $this.attr('data-parent') )
+            {
+
+                var $thisParentAttrVal = $this.attr('data-parent');
+
+                 //hide/reset all levels
+                 $allSections.addClass('hidden');
+                 
+                 //show content
+                $('.hamburger-content>.section[data-link-to-content="'+$thisParentAttrVal+'"]').removeClass('hidden');
+
+                //scroll to top
+                $('.hamburger-content > *').scrollTop(0);
+
+            }else
+            //else all revert back to level 1
+            {
+
+                 //hide/reset all levels
+                 $allSections.addClass('hidden');
+                 
+                 //show content
+                $('.hamburger-content>.section.level-1').removeClass('hidden');
+
+                //scroll to top
+                $('.hamburger-content > *').scrollTop(0);
+
+            }
+
+
+        });
+
+
+        //when click on [data-link-back-to] urls
+        // $("[data-link-back-to-level-2]").on( "click",  function() { 
+        //     console.log('click');
+        //     var $this = $(this);
+
+        //     var $allSections = $('.hamburger-content>.section');
+        //     var $allSectionsLevel2 = $('.hamburger-content>.section.level-2');
+        //     var $allSectionsLevel2AttrVal = $allSectionsLevel2.attr('data-link-back-to-level-2');
+
+            
+        //     if ($('.level-2').index($allSectionsLevel2AttrVal) >= 0){
+        //         console.log('yes');
+        //     }
+
+        //     //var $thisAttrVal = $this.attr('data-link-back-to');
+
+        //      //hide/reset all levels
+        //      $allSections.addClass('hidden');
+             
+        //      //show content
+        //     $('.hamburger-content>.section.level-2').removeClass('hidden');
+
+        //     //scroll to top
+        //     $('.hamburger-content > *').scrollTop(0);
+
+
+        // });
+        
+
+
+
+
     });
 })();
 
@@ -536,7 +637,7 @@ $(window).on('load', function() {
             minWidth: 0
         });
 
-        console.log('onload');
+        //console.log('onload');
 
     }
 
@@ -795,3 +896,165 @@ $(window).on('load', function() {
 
     });
 })();
+
+
+
+
+// ----------------------------------------------------------------------
+// test homebanner video
+// ----------------------------------------------------------------------
+
+(function () {
+    "use strict";
+    $(document).ready(function () {
+
+        var slideWrapper = $(".main-slider"),
+        iframes = slideWrapper.find('.embed-player'),
+        lazyImages = slideWrapper.find('.slide-image'),
+        lazyCounter = 0;
+    
+    // POST commands to YouTube or Vimeo API
+    function postMessageToPlayer(player, command){
+      if (player == null || command == null) return;
+      player.contentWindow.postMessage(JSON.stringify(command), "*");
+    }
+    
+    // When the slide is changing
+    function playPauseVideo(slick, control){
+      var currentSlide, slideType, startTime, player, video;
+    
+      currentSlide = slick.find(".slick-current");
+      slideType = currentSlide.attr("class").split(" ")[1];
+      player = currentSlide.find("iframe").get(0);
+      startTime = currentSlide.data("video-start");
+    
+      if (slideType === "vimeo") {
+        switch (control) {
+          case "play":
+            if ((startTime != null && startTime > 0 ) && !currentSlide.hasClass('started')) {
+              currentSlide.addClass('started');
+              postMessageToPlayer(player, {
+                "method": "setCurrentTime",
+                "value" : startTime
+              });
+            }
+            postMessageToPlayer(player, {
+              "method": "play",
+              "value" : 1
+            });
+            break;
+          case "pause":
+            postMessageToPlayer(player, {
+              "method": "pause",
+              "value": 1
+            });
+            break;
+        }
+      } else if (slideType === "youtube") {
+        switch (control) {
+          case "play":
+            postMessageToPlayer(player, {
+              "event": "command",
+              "func": "mute"
+            });
+            postMessageToPlayer(player, {
+              "event": "command",
+              "func": "playVideo"
+            });
+            break;
+          case "pause":
+            postMessageToPlayer(player, {
+              "event": "command",
+              "func": "pauseVideo"
+            });  
+            break;
+        }
+      } else if (slideType === "video") {
+        video = currentSlide.children("video").get(0);
+        if (video != null) {
+          if (control === "play"){
+            video.play();
+          } else {
+            video.pause();
+          }
+        }
+      }
+    }
+    
+    // Resize player
+    function resizePlayer(iframes, ratio) {
+      if (!iframes[0]) return;
+      var win = $(".main-slider"),
+          width = win.width(),
+          playerWidth,
+          height = win.height(),
+          playerHeight,
+          ratio = ratio || 16/9;
+    
+      iframes.each(function(){
+        var current = $(this);
+        if (width / ratio < height) {
+          playerWidth = Math.ceil(height * ratio);
+          current.width(playerWidth).height(height).css({
+            left: (width - playerWidth) / 2,
+             top: 0
+            });
+        } else {
+          playerHeight = Math.ceil(width / ratio);
+          current.width(width).height(playerHeight).css({
+            left: 0,
+            top: (height - playerHeight) / 2
+          });
+        }
+      });
+    }
+    
+    // DOM Ready
+    $(function() {
+      // Initialize
+      slideWrapper.on("init", function(slick){
+        slick = $(slick.currentTarget);
+        setTimeout(function(){
+          playPauseVideo(slick,"play");
+        }, 1000);
+        resizePlayer(iframes, 16/9);
+      });
+      slideWrapper.on("beforeChange", function(event, slick) {
+        slick = $(slick.$slider);
+        playPauseVideo(slick,"pause");
+      });
+      slideWrapper.on("afterChange", function(event, slick) {
+        slick = $(slick.$slider);
+        playPauseVideo(slick,"play");
+      });
+      slideWrapper.on("lazyLoaded", function(event, slick, image, imageSource) {
+        lazyCounter++;
+        if (lazyCounter === lazyImages.length){
+          lazyImages.addClass('show');
+          // slideWrapper.slick("slickPlay");
+        }
+      });
+    
+      //start the slider
+      slideWrapper.slick({
+        // fade:true,
+        autoplaySpeed:4000,
+       // autoplay:false,
+        lazyLoad:"progressive",
+       // speed:600,
+        // arrows:true,
+        // dots:true,
+        //cssEase:"cubic-bezier(0.87, 0.03, 0.41, 0.9)"
+      });
+    });
+    
+    // Resize event
+    $(window).on("resize.slickVideoPlayer", function(){  
+      resizePlayer(iframes, 16/9);
+    });
+
+
+    });
+})();
+
+
