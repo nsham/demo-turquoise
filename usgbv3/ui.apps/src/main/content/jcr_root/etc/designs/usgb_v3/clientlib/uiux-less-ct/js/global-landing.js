cea4e6070e19698ee5c3d961c97d22c9d9dfa8c1
Tan Chuan Theng <tctheng02175@hotmail.com>
2018-07-12 00:08:55 +0800

    global landing page logic exception

--- usgbv3/ui.apps/src/main/content/jcr_root/etc/designs/usgb_v3/clientlib/uiux-less-ct/js/global-landing.js
+++ usgbv3/ui.apps/src/main/content/jcr_root/etc/designs/usgb_v3/clientlib/uiux-less-ct/js/global-landing.js
@@ -3,7 +3,9 @@
 // ----------------------------------------------------------------------
 (function(){
     "use strict";
-    $(document).ready(function(){
-        $(".carousel-feature-card").slick();
+    $(document).ready(function () {
+        if($(".carousel-feature-card").length > 0){
+            $(".carousel-feature-card").slick();
+        }
     });
 })();
