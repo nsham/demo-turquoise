ccf9cd5ca4bdcb1eae01f53a6461bbb27788d96e
Tan Chuan Theng <tctheng02175@hotmail.com>
2018-07-13 16:24:56 +0800

    where to buy json and utils

--- usgbv3/ui.apps/src/main/content/jcr_root/etc/designs/usgb_v3/clientlib/uiux-less-ct/js/where-to-buy.js
+++ usgbv3/ui.apps/src/main/content/jcr_root/etc/designs/usgb_v3/clientlib/uiux-less-ct/js/where-to-buy.js
@@ -495,21 +495,7 @@
             });
         }
 
-        $.fn.serializeObject = function(){
-            var o = {};
-            var a = this.serializeArray();
-            $.each(a, function() {
-                if (o[this.name] !== undefined) {
-                    if (!o[this.name].push) {
-                        o[this.name] = [o[this.name]];
-                    }
-                    o[this.name].push(this.value || '');
-                } else {
-                    o[this.name] = this.value || '';
-                }
-            });
-            return o;
-        };
+        
 
         function triggerClick(index) {
             console.log(markers[index]);
